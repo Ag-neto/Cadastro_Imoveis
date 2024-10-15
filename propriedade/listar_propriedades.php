3517d2f93741426589a951ba0bed194a224760ba
Micael Lucas <109772685+MicaelLucasDT@users.noreply.github.com>
2024-10-15 10:37:01 -0300

    up

--- propriedade/listar_propriedades.php
+++ propriedade/listar_propriedades.php
@@ -50,10 +50,11 @@
                 $busca = $conn->real_escape_string($busca); // Evita SQL Injection
 
                 // Prepara a consulta SQL
-                $sql = "SELECT p.*, t.nome_tipo, l.nome_cidade, s.nome_situacao 
+                $sql = "SELECT p.*, t.nome_tipo, l.nome_cidade, s.nome_situacao, e.sigla 
                         FROM propriedade p 
                         JOIN tipo_prop t ON p.id_tipo_prop = t.id_tipo_prop
                         JOIN localizacao l ON p.id_localizacao = l.idlocalizacao
+                        JOIN estados e ON l.id_estado = e.id_estado
                         JOIN situacao s ON p.id_situacao = s.id_situacao";
 
                 // Adiciona condição de busca se houver
@@ -71,7 +72,7 @@
                         echo '<td>' . $row['idpropriedade'] . '</td>'; // Supondo que o ID da propriedade seja 'id_propriedade'
                         echo '<td>' . $row['nome_propriedade'] . '</td>';
                         echo '<td>' . $row['nome_tipo'] . '</td>';
-                        echo '<td>' . $row['nome_cidade'] . '</td>';
+                        echo '<td>' . $row['nome_cidade'] . " - " . $row['sigla'] .'</td>';
                         echo '<td>' . number_format($row['valor_adquirido'], 2, ',', '.') . '</td>'; // Formata o valor
                         echo '<td>' . $row['nome_situacao'] . '</td>';
                         echo '<td><a href="detalhes_propriedade.php?id=' . $row['idpropriedade'] . '">Ver Detalhes</a></td>';
